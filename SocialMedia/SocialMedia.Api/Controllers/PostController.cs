56e25f7f58e26577b6a6ddcf0c2163b890f75424
Oscar Rueda <oscar_rueda@hotmail.com>
2022-09-24 16:38:33 -0500

    First Commit Most basic implementation witout any pattern only Clean Architecture
    Signed-off-by: Oscar Rueda <oscar_rueda@hotmail.com>

---
+++ SocialMedia/SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infrastructure.Repositories;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var posts = new PostRepository().GetPosts();
+
+            return Ok(posts);
+        }
+    }
+}
---
+++ SocialMedia/SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,26 @@
+﻿using SocialMedia.Core.Entities;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public PostRepository()
+        {
+
+        }
+
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post()
+            {
+                PostId = x,
+                Date = DateTime.UtcNow,
+                Description = $"Descripcion {x}",
+                Image ="http://unaimagen.png",
+                UserId = x * 2
+            });
+
+            return posts;
+        }
+    }
+}
