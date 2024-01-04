eadab9ac0b734dd72098d8f1513114466b8ebd92
Oscar Alonso Rueda Ruiz <138699802+oruedarr@users.noreply.github.com>
2024-01-03 20:47:04 -0500

    Fix entity default id

--- SocialMedia/SocialMedia.Core/Entities/BaseEntity.cs
+++ SocialMedia/SocialMedia.Core/Entities/BaseEntity.cs
@@ -4,7 +4,7 @@ namespace SocialMedia.Core.Entities
 {
     public abstract class BaseEntity
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public string Responsable { get; set; }
     }
